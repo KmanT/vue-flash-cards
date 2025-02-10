5702a5c3b698ecc671d07cfdc4ce4774e450d675
Kyle Turske <kyle.turske@protonmail.com>
2025-02-10 08:55:12 -0500

    feat(src/views/*): HomeView and AddCardView

---
+++ src/views/AddCardView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="add-card">
+    <h1>Add Card</h1>
+    <AddCardForm />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import AddCardForm from '@/components/AddCardForm.vue'
+</script>
+
+<style>
+.add-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>Vue Flash Cards</h1>
+    <h2 v-if="fcStore.hasAnsweredAll">You answered all of the questions!</h2>
+    <CurrentScore />
+    <FlashCardContainer />
+    <ScoreControls />
+    <EndQuizModal />
+  </main>
+</template>
+
+<script setup lang="ts">
+import FlashCardContainer from '@/components/FlashCardContainer.vue'
+import CurrentScore from '@/components/CurrentScore.vue'
+import ScoreControls from '@/components/ScoreControls.vue'
+import { useFlashCardStore } from '@/stores/flash-cards'
+import EndQuizModal from '@/components/EndQuizModal.vue'
+
+const fcStore = useFlashCardStore()
+</script>
