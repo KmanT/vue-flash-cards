2ee621b37d832e213be7820b058e132cfe1bf301
Kyle Turske <kyle.turske@protonmail.com>
2025-02-10 08:53:47 -0500

    feat(src/router/index.ts): base router

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AddCardView from '../views/AddCardView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/add-card',
+      name: 'add-card',
+      component: AddCardView,
+    },
+  ],
+})
+
+export default router
